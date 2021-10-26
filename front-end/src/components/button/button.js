3a127a7e8912e734cae60158a247e1ab24667756
AyeshaZfr <az1866@nyu.edu>
2021-10-26 12:38:27 -0400

    button

--- front-end/src/components/button/button.js
+++ front-end/src/components/button/button.js
@@ -1,14 +1,12 @@
 
 import { styled } from '@mui/material/styles';
-import { purple } from '@mui/material/colors';
 import Button from '@mui/material/Button';
-// import "./button.css";
 
 
 
 const ButtonUI = styled(Button)(() => ({
     color: '#000000',
-    fontWeight: 'bold',
+    padding:0,
     textTransform: 'none',
     borderColor: '#000000',
     backgroundColor: '#89dc9f',
