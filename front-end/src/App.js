c8220dd5576c949a32ba2abdb787e41f3b290403
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2021-10-26 19:37:03 -0400

    Created Landing Page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import LogoSection from './components/LogoSection/LogoSection';
+import MainCarousel from './components/Carousel/MainCarousel';
+import AboutSection from './components/AboutSection/AboutSection';
+import Partners from './components/Partners/Partners';
+import Footer from './components/Footer/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LogoSection/>
+      <MainCarousel/>
+      <AboutSection/>
+      <Partners/>
+      <Footer/>
     </div>
   );
 }
