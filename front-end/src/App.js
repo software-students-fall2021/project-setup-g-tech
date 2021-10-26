35133c5fda77d1f5814758a45f84ef4fbcd4c337
yusra-khan <yusrakhawar2001@gmail.com>
2021-10-26 03:25:00 -0400

    set up routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,24 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+import Checkout from './components/checkout-page/Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+
+        <Route path="/checkout">
+          <Checkout/>
+        </Route>
+
+        
+        <Route path="/">
+
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
