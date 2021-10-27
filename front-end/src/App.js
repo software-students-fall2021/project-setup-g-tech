5c11e04d99b0f9889c092cf32024f1c85788652e
ishmalkhalid <ishmal2000@gmail.com>
2021-10-26 23:59:29 -0400

    signin set iup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,5 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-
-import logo from './logo.svg';
 import './App.css';
 import LogoSection from './components/LogoSection/LogoSection';
 import MainCarousel from './components/Carousel/MainCarousel';
@@ -11,32 +9,39 @@ import Footer from './components/Footer/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Checkout from './components/checkout-page/Checkout';
 import Register from './components/register-page/Register';
+import Signin from './components/Sign-in/Signin';
 import MenuPage from './components/menu-page/menu-page';
 
 function App() {
   return (
-      <BrowserRouter>
-        <Switch>
+    <BrowserRouter>
+      <Switch>
 
-          <Route path="/checkout">
-            <Checkout/>
-          </Route>
-          <Route path="/menu">
-            <MenuPage/>
-          </Route>
+        <Route path="/checkout">
+          <Checkout/>
+        </Route>
+
+        <Route path="/menu">
+          <MenuPage/>
+        </Route>
 
         <Route path="/register">
           <Register/>
         </Route>
+
+        <Route path="/signin">
+          <Signin/>
+        </Route>
           
-          <Route path="/">
-              <LogoSection/>
-              <MainCarousel/>
-              <AboutSection/>
-              <Partners/>
-              <Footer/>
-          </Route>
-          </Switch>
+        <Route path="/">
+            <LogoSection/>
+            <MainCarousel/>
+            <AboutSection/>
+            <Partners/>
+            <Footer/>
+        </Route>
+
+        </Switch>
       </BrowserRouter>
   );
 }
