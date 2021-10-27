2d2730c5b42c7ef9114a30686f052065a408e5d0
yusra-khan <yusrakhawar2001@gmail.com>
2021-10-26 20:48:11 -0400

    header-tab styling fix

---
+++ front-end/src/components/bill-item/Billitem.js
@@ -0,0 +1,27 @@
+import './Billitem.css';
+
+const Billitem = (props) => {
+    let pretotal = parseFloat(props.price);
+    // console.log(pretotal);
+    // pretotal = toString(pretotal);
+    return (
+        <>
+            <div className='row justify-content-center table-row'>
+                <div className='col-4'>
+                    <p className='table-item quantity'>{props.quantity}x</p>
+                </div>
+                <div className='col-2'>
+                    <p className='table-item item-name'>{props.itemName}</p>
+                </div>
+                <div className='col-4'>
+                    <p className='table-item price'>${props.price}</p>
+                </div>
+            </div>
+            <div className='pretotal'>
+                <p>{toString(pretotal)}</p>
+            </div>
+        </>
+    );
+}
+
+export default Billitem;
