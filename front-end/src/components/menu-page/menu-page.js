f43ce30c4d9b3e65f90e63362bedb60331ef2256
AyeshaZfr <az1866@nyu.edu>
2021-11-01 14:24:46 -0400

    commit

--- front-end/src/components/menu-page/menu-page.js
+++ front-end/src/components/menu-page/menu-page.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './menu-page.css';
 import ImageCont from './bg-image';
 import HorizontalMenu from '../scroll-bar/scroll-bar';
 import MenuCard from '../menu-card/menu-card';
 import HeaderTab from '../header-tab/HeaderTab';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
 
 const MenuPage = () => {
     const menuItems = ['Appetizers', 'Sides', 'Drinks', 'Dessert', 'Entrée', 'Sauces'];
@@ -18,7 +21,7 @@ const MenuPage = () => {
 
     return (
         <>
-            <HeaderTab pageTitle="Restaurant Name"/>
+            <HeaderTab pageTitle="Saverie"/>
 
             <ImageCont img = 'https://image.shutterstock.com/shutterstock/photos/1113487829/display_1500/stock-photo-food-dine-fine-black-plate-dish-exclusive-elegant-modern-appetizer-meat-small-dinner-luxury-1113487829.jpg'/>
             <HorizontalMenu items = {menuItems}/>
