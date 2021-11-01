1dfc044459f596dfc58d0d55ac9df85df56b9145
ishmalkhalid <ishmal2000@gmail.com>
2021-11-01 17:27:27 -0400

    importing middleware

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,12 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
-// we will put some server logic here later...
+
+// import some useful middleware
+const multer = require("multer") // middleware to handle HTTP POST requests with file uploads
+const axios = require("axios") // middleware for making requests to APIs
+require("dotenv").config({ silent: true }) // load environmental variables from a hidden file named .env
+const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
+
 // export the express app we created to make it available to other modules
 module.exports = app
