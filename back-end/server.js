2cc25eae4babdb426be14619d0c2fa14ee5293b3
ishmalkhalid <ishmal2000@gmail.com>
2021-11-08 18:37:07 -0500

    sign in post request and unit testing

--- back-end/server.js
+++ back-end/server.js
@@ -3,8 +3,89 @@ const server = require("./app") // load up the web server
 const port = 3000 // the port to listen to for incoming requests
 // call express's listen function to start listening to the port
 const listener = server.listen(port, function () {
+<<<<<<< Updated upstream
   console.log(`Server running on port: ${port}`)
 })
+=======
+  console.log(`Server running on port: ${port}`);
+});
+
+// route handling
+server.get("/usermenu", (req, res, next) => {
+  if (req.query.user == "mockData") {
+    axios
+      .get(`https://my.api.mockaroo.com/restaurant_info.json?key=4aedac00`)
+      .then((apiRes) => res.json(apiRes.data));
+  } else {
+    res.status(404);
+    next();
+  }
+});
+
+server.get("/saveddistributors", (req, res, next) => {
+  if (req.query.user == "mockData") {
+    axios
+      .get(`https://my.api.mockaroo.com/restaurant_info.json?key=4aedac00`)
+      .then((apiRes) => res.json(apiRes.data));
+  } else {
+    res.status(404);
+    next();
+  }
+});
+
+//register authentication
+server.post("/register-submit", function (req, res) {
+  if (
+    req.body.first_name &&
+    req.body.last_name &&
+    req.body.email &&
+    req.body.password &&
+    req.body.repassword
+  ) {
+    const data = {
+      status: "true",
+      message: "The data has been entered to the database",
+      your_data: {
+        name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password,
+        repassword: req.body.repassword,
+      },
+    };
+    console.log(data);
+    res.status(200);
+    res.redirect("http://localhost:3000/signin");
+  } else {
+    res.status(400);
+    res.redirect("http://localhost:3000/register");
+  }
+});
+
+//sign in suthentication
+server.post("/signin-submit", function (req, res) {
+  if (
+    req.body.email &&
+    req.body.password
+  ) {
+    const data = {
+      status: "true",
+      message: "The data has been posted",
+      your_data: {
+        email: req.body.email,
+        password: req.body.password      },
+    };
+    console.log(data);
+    res.status(200);
+    res.redirect("http://localhost:3000/usermenu");
+  } else {
+    res.status(400);
+    res.redirect("http://localhost:3000/signin");
+  }
+});
+
+
+>>>>>>> Stashed changes
 // a function to stop listening to the port
 const close = () => {
   listener.close()
---
+++ back-end/test/server.spec.js
@@ -0,0 +1,119 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+var expect = require('chai').expect;
+var app = require('../server');
+
+// Register testing
+describe('Testing the POST Call', function(){
+    it('API status code should return 200', function(done){
+        chai.request(app)
+              .post('/register-submit')
+              .send({
+                    "first_name": "Zunair",
+                    "last_name": "Viqar",
+                    "email":"zunairv@gmail.com",
+                    "password":"saverieisawesome",
+                    "repassword":"saverieisawesome"
+              }).end((err,res)=>{
+                 if(err) done(err);
+                 expect(res).to.have.redirectTo("http://localhost:3000/signin");
+                //  console.log(JSON.stringify(res,"",2));
+                 done();
+              })
+      })
+      it('API status code should return 400 for a missing value', function(done){
+        chai.request(app)
+              .post('/register-submit')
+              .send({
+                    "first_name": "Zunair",
+                    "last_name": "Viqar",
+                    "email":"zunairv@gmail.com",
+                    "password":"saverieisawesome",
+              }).end((err,res)=>{
+                 if(err) done(err);
+                 expect(res).to.have.redirectTo("http://localhost:3000/register");
+                //  console.log(JSON.stringify(res,"",2));
+                 done();
+              })
+      })
+
+});
+
+// Sign in testing
+describe('Testing the POST call in sign in', function(){
+    it('API status code should return 200', function(done){
+        chai.request(app)
+              .post('/signin-submit')
+              .send({
+                    "email":"ishmal@gmail.com",
+                    "password":"saverie4lyfe"
+              }).end((err,res)=>{
+                 if(err) done(err);
+                 expect(res).to.have.redirectTo("http://localhost:3000/usermenu");
+                //  console.log(JSON.stringify(res,"",2));
+                 done();
+              })
+      })
+      it('API status code should return 400 for a missing value', function(done){
+        chai.request(app)
+              .post('/signin-submit')
+              .send({
+                "email":"ishmal@gmail.com",
+              }).end((err,res)=>{
+                 if(err) done(err);
+                 expect(res).to.have.redirectTo("http://localhost:3000/signin");
+                //  console.log(JSON.stringify(res,"",2));
+                 done();
+              })
+      })
+
+});
+
+
+// User menu testing
+describe('Testing user menu', () => {
+    it('API status code returns 200', (done) => {
+        chai.request(app)
+            .get('/usermenu')
+            .query({user: 'mockData'})
+            .end((err, res) => {
+                expect(err).to.be.null
+                expect(res).to.have.status(200)
+                done()
+            })
+    })
+    it('API status code returns 404 for missing/incorrect data address', (done) => {
+        chai.request(app)
+            .get('/usermenu')
+            .query({user: 'none'})
+            .end((err, res) => {
+                expect(res).to.have.status(404)
+                done()
+            })
+    })
+  });
+
+// Saved distributors testing
+describe('Testing saveddistributors', () => {
+    it('API status code returns 200', (done) => {
+        chai.request(app)
+            .get('/saveddistributors')
+            .query({user: 'mockData'})
+            .end((err, res) => {
+                expect(err).to.be.null
+                expect(res).to.have.status(200)
+                done()
+            })
+    })
+    it('API status code returns 404 for missing/incorrect data address', (done) => {
+        chai.request(app)
+            .get('/saveddistributors')
+            .query({user: 'none'})
+            .end((err, res) => {
+                expect(res).to.have.status(404)
+                done()
+            })
+    })
+})
+
