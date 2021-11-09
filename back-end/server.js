f44de450bcfae988bcd8658596c91ccf14397037
jingjing.liu2 <jl9358@nyu.edu>
2021-11-08 23:32:51 -0500

    updated server.js with order history

--- back-end/server.js
+++ back-end/server.js
@@ -12,10 +12,6 @@ const port = 3001;
 
 // call express's listen function to start listening to the port
 const listener = server.listen(port, function () {
-
-  console.log(`Server running on port: ${port}`)
-})
-
   console.log(`Server running on port: ${port}`);
 });
 
@@ -24,6 +20,19 @@ server.get("/usermenu", (req, res, next) => {
   if (req.query.user == "mockData") {
     axios
       .get(`https://my.api.mockaroo.com/restaurant_info.json?key=4aedac00`)
+      .then((apiRes) => res.json(apiRes.data));
+  } else {
+    res.status(404);
+    next();
+  }
+});
+
+server.get("/orderhistorypage", (req, res, next) => {
+  if (req.query.user == "mockData") {
+    axios
+      .get(
+        `https://my.api.mockaroo.com/mock_user_order_history.json?key=0b54f900`
+      )
       .then((apiRes) => res.json(apiRes.data));
   } else {
     res.status(404);
@@ -46,7 +55,9 @@ server.get("/saveddistributors", (req, res, next) => {
 server.get("/menu", (req, res, next) => {
   if (req.query.user == "mockData") {
     axios
-      .get(`https://my.api.mockaroo.com/restaurant_menu.json?key=84c7cbc0&__method=POST`)
+      .get(
+        `https://my.api.mockaroo.com/restaurant_menu.json?key=84c7cbc0&__method=POST`
+      )
       .then((apiRes) => res.json(apiRes.data));
   } else {
     res.status(404);
@@ -84,16 +95,14 @@ server.post("/register-submit", function (req, res) {
 
 //sign in suthentication
 server.post("/signin-submit", function (req, res) {
-  if (
-    req.body.email &&
-    req.body.password
-  ) {
+  if (req.body.email && req.body.password) {
     const data = {
       status: "true",
       message: "The data has been posted",
       your_data: {
         email: req.body.email,
-        password: req.body.password      },
+        password: req.body.password,
+      },
     };
     console.log(data);
     res.status(200);
